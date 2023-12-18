3c8eb58034f623085dc7b016e5c76f1211ccbc6d
GabrielTI2001 <damatagabriel1011@gmail.com>
2023-12-18 17:17:48 -0300

    Update 18.12.2023

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from rest_framework.response import Response
+from .serializers import serializerProfile
+from .models import Profile
 
 # Create your views here.
+class ProfileView(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = serializerProfile
+    permission_classes = [permissions.AllowAny]
