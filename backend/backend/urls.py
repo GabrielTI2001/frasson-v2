f491b4ac8e38bc3e3493f62d6a30e05035c11a8f
GabrielTI2001 <damatagabriel1011@gmail.com>
2023-12-18 17:06:00 -0300

    Update 18.12.2023

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import static
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cadastro/', include('cadastro.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
 ]
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    pipefy_id = models.BigIntegerField(null=True, verbose_name='Id do Usuário no Pipefy')
+    #pipefy_token = models.CharField(max_length=255, null=True, verbose_name='Token de acesso pessoal Pipefy')
+    job_function = models.CharField(max_length=255, null=True, verbose_name='Cargo ou Função')
+    cpf =  models.CharField(max_length=255, null=True, verbose_name='CPF')
+    bio = models.TextField(default=None, null=True, verbose_name='Biografia')
+    birthday = models.DateField(null=True, verbose_name='Data Nascimento')
+    contato_celular =  models.CharField(max_length=255, null=True, verbose_name='Contato Celular')
+    avatar = models.FileField(default='avatars/users/default-avatar.jpg', upload_to='avatars/users')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name_plural = 'User Profile'
+    def __str__(self):
+        return self.user.first_name
