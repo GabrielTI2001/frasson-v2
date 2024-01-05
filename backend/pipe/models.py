13464dfda92625d5be115b8bef63714c5cb26210
GabrielTI2001 <damatagabriel1011@gmail.com>
2024-01-05 10:00:47 -0300

    At 05.01.2024

--- backend/pipe/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-# from cadastro.models import Cadastro_Pessoal, Instituicoes_Razao_Social
-
-# Create your models here.
-
-  
--- backend/pipeline/apps.py
+++ backend/pipeline/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PipeConfig(AppConfig):
+class PipelineConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pipe'
+    name = 'pipeline'
